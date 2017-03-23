31666a8e376b1f3c461cb8fe3c9d35bb563ef11f
LOTCHON9 <inile1995@gmail.com>
2017-03-23 12:41:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sdu/lotchon9/animalforfunn/Gamee.java
@@ -0,0 +1,13 @@
+package com.example.sdu.lotchon9.animalforfunn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Gamee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gamee);
+    }
+}
